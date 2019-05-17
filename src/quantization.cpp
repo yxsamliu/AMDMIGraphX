3217a9367e8e36e4265a6faba8e85eb7c8fc5c34
Shucai Xiao <Shucai.Xiao@amd.com>
2019-05-17 11:03:46 -0500

    code backup

--- src/quantization.cpp
+++ src/quantization.cpp
@@ -131,7 +131,7 @@ void quantize_int8(program& prog, const std::vector<std::string>& ins_names)
     }
 
     // tmp value used just testing
-    std::vector<std::pair<float, float>> int8_param{{1.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 0.0f}};
+    std::vector<std::pair<float, float>> int8_param{{64.0f, 0.0f}, {64.0f, 0.0f}, {128.0f, 0.0f}};
 
     std::unordered_map<instruction_ref, instruction_ref> map_quant_ins;
     for(auto ins : iterator_for(prog))
@@ -337,7 +337,7 @@ void quantize_int8(program& prog, const std::vector<std::string>& ins_names)
             auto group         = conv_op.group;
             auto adjust_factor = 1.0 / (int8_param[0].first * int8_param[1].first);
 
-            shape quant_shape = compute_shape(op::quant_convolution{}, converted_inputs);
+            shape quant_shape = compute_shape(op::quant_convolution{padding, stride, dilation, padding_mode, group}, converted_inputs);
             std::vector<float> vec_factor(quant_shape.elements(), adjust_factor);
             auto fl = prog.add_literal(literal{{orig_type, quant_shape.lens()}, vec_factor});
             if(quant_shape.type() == orig_type)
@@ -351,7 +351,7 @@ void quantize_int8(program& prog, const std::vector<std::string>& ins_names)
                 }
                 else
                 {
-                    auto quant_conv = prog.replace_instruction(
+                    auto quant_conv = prog.insert_instruction(
                         ins,
                         op::quant_convolution{padding, stride, dilation, padding_mode, group},
                         converted_inputs);
