5d833b632192863bc049e97a1aab3a572e9851e9
Paul <pfultz2@yahoo.com>
2020-02-07 12:13:13 -0600

    Formatting

--- src/propagate_copy.cpp
+++ src/propagate_copy.cpp
@@ -10,14 +10,14 @@ inline namespace MIGRAPHX_INLINE_NS {
 
 bool single_use(instruction_ref alias, instruction_ref ins)
 {
-    if (ins->outputs().size() > 1)
+    if(ins->outputs().size() > 1)
         return false;
-    if (alias == ins)
+    if(alias == ins)
         return true;
-    for(auto input:ins->inputs())
+    for(auto input : ins->inputs())
     {
         auto i = instruction::get_output_alias(input);
-        if (i != alias)
+        if(i != alias)
             continue;
         if(not single_use(i, input))
             return false;
@@ -32,10 +32,10 @@ void propagate_copy::apply(program& p) const
         if(ins->name() != copy)
             continue;
         auto input = ins->inputs().front();
-        if (ins->get_shape() != input->get_shape())
+        if(ins->get_shape() != input->get_shape())
             continue;
-        auto i     = instruction::get_output_alias(input);
-        if (i->name()[0] == '@')
+        auto i = instruction::get_output_alias(input);
+        if(i->name()[0] == '@')
             continue;
         if(not single_use(i, input))
             continue;
