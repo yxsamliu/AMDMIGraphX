cb81aec356ad8efe5b982da2cb09b7e240b6c3b0
Paul <pfultz2@yahoo.com>
2020-02-12 10:50:53 -0600

    Skip identity

--- src/propagate_copy.cpp
+++ src/propagate_copy.cpp
@@ -14,12 +14,19 @@ bool single_use(instruction_ref alias, instruction_ref ins)
         return false;
     if(alias == ins)
         return true;
-    for(auto input : ins->inputs())
-    {
+    auto check_arg = [&](auto input) -> bool {
         auto i = instruction::get_output_alias(input);
         if(i != alias)
-            continue;
+            return true;
         if(not single_use(i, input))
+            return false;
+        return true;
+    };
+    if (ins->name() == "identity")
+        return check_arg(ins->inputs().front());
+    for(auto input : ins->inputs())
+    {
+        if (not check_arg(input))
             return false;
     }
     return true;
@@ -39,7 +46,7 @@ void propagate_copy::apply(program& p) const
             continue;
         if(not single_use(i, input))
             continue;
-        p.replace_instruction(ins, op::identity{}, i, input);
+        p.replace_instruction(ins, op::identity{}, input);
     }
 }
 
