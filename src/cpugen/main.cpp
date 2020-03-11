da0ccf623aa3f6151a6214a804b7c0ba76ed4151
Paul <pfultz2@yahoo.com>
2020-03-11 17:01:15 -0500

    Formatting

--- src/cpugen/main.cpp
+++ src/cpugen/main.cpp
@@ -20,45 +20,37 @@
 
 struct gen_target
 {
-    std::string name() const
+    std::string name() const { return "cpu-generate"; }
+    std::vector<migraphx::pass> get_passes(migraphx::context& ctx,
+                                           const migraphx::compile_options&) const
     {
-        return "cpu-generate";
-    }
-    std::vector<migraphx::pass> get_passes(migraphx::context& ctx, const migraphx::compile_options&) const
-    {
-        return 
-        {
-            migraphx::decompose{},
-            migraphx::dead_code_elimination{},
-            migraphx::simplify_reshapes{},
-            migraphx::dead_code_elimination{},
-            migraphx::eliminate_identity{},
-            migraphx::eliminate_pad{},
-            migraphx::dead_code_elimination{},
-            migraphx::rewrite_batchnorm{},
-            migraphx::dead_code_elimination{},
-            migraphx::rewrite_rnn{},
-            migraphx::rewrite_pooling{},
-            migraphx::dead_code_elimination{},
-            migraphx::eliminate_common_subexpression{},
-            migraphx::dead_code_elimination{},
-            migraphx::simplify_algebra{},
-            migraphx::dead_code_elimination{},
-            migraphx::auto_contiguous{},
-            migraphx::simplify_reshapes{},
-            migraphx::dead_code_elimination{},
-            migraphx::propagate_constant{},
-            migraphx::dead_code_elimination{}
-        };
+        return {migraphx::decompose{},
+                migraphx::dead_code_elimination{},
+                migraphx::simplify_reshapes{},
+                migraphx::dead_code_elimination{},
+                migraphx::eliminate_identity{},
+                migraphx::eliminate_pad{},
+                migraphx::dead_code_elimination{},
+                migraphx::rewrite_batchnorm{},
+                migraphx::dead_code_elimination{},
+                migraphx::rewrite_rnn{},
+                migraphx::rewrite_pooling{},
+                migraphx::dead_code_elimination{},
+                migraphx::eliminate_common_subexpression{},
+                migraphx::dead_code_elimination{},
+                migraphx::simplify_algebra{},
+                migraphx::dead_code_elimination{},
+                migraphx::auto_contiguous{},
+                migraphx::simplify_reshapes{},
+                migraphx::dead_code_elimination{},
+                migraphx::propagate_constant{},
+                migraphx::dead_code_elimination{}};
     }
     migraphx::context get_context() const { return migraphx::context{}; }
 
     migraphx::argument copy_to(const migraphx::argument& arg) const { return arg; }
     migraphx::argument copy_from(const migraphx::argument& arg) const { return arg; }
-    migraphx::argument allocate(const migraphx::shape& s) const
-    {
-        return {};
-    }
+    migraphx::argument allocate(const migraphx::shape& s) const { return {}; }
 };
 
 std::string preamble = R"CODE(
@@ -66,14 +58,13 @@ std::string preamble = R"CODE(
 
 )CODE";
 
-
-template<class R, class F>
+template <class R, class F>
 void delim(std::ostream& os, std::string d, R&& r, F f)
 {
     bool first = true;
-    for(auto&& x:r)
+    for(auto&& x : r)
     {
-        if (first)
+        if(first)
             first = false;
         else
             os << d;
@@ -83,16 +74,14 @@ void delim(std::ostream& os, std::string d, R&& r, F f)
 
 struct generator
 {
-    migraphx::program * prog;
-    std::unordered_map<std::string, std::function<std::string(migraphx::instruction_ref)>> apply_map{};
+    migraphx::program* prog;
+    std::unordered_map<std::string, std::function<std::string(migraphx::instruction_ref)>>
+        apply_map{};
 
     std::stringstream fs{};
     std::size_t function_count = 0;
 
-    void init()
-    {
-
-    }
+    void init() {}
 
     struct param
     {
@@ -100,13 +89,14 @@ struct generator
         std::string type;
     };
 
-    std::string create_function(std::vector<param> params, std::string return_type, std::string content)
+    std::string
+    create_function(std::vector<param> params, std::string return_type, std::string content)
     {
         function_count++;
         std::string name = std::to_string(function_count);
         fs << return_type << " " << name << "(";
         char delim = '(';
-        for(auto&& p:params)
+        for(auto&& p : params)
         {
             fs << delim << p.type << " " << p.name;
             delim = ',';
@@ -120,23 +110,22 @@ struct generator
         return "std::array<" + name + ", " + std::to_string(n) + ">";
     }
 
-
     std::string to_array_type(const migraphx::shape& s)
     {
         return to_array_type(s.type_string(), s.elements());
     }
 
-
-    std::string create_pointwise(std::vector<migraphx::shape> inputs, migraphx::shape output, std::string op)
+    std::string
+    create_pointwise(std::vector<migraphx::shape> inputs, migraphx::shape output, std::string op)
     {
-        if (inputs.empty())
+        if(inputs.empty())
             return "";
         std::size_t n = inputs.front().elements();
         std::stringstream ss;
         ss << to_array_type(output) << " result;\n";
         ss << "for(std::size_t i = 0; i < " + std::to_string(n) + "; i++) {\n";
         std::size_t x = 0;
-        for(auto input:inputs)
+        for(auto input : inputs)
         {
             std::string index = "i";
             // if (not input.standard())
@@ -173,7 +162,7 @@ struct generator
         auto params = prog->get_parameter_shapes();
         delim(ss, ", ", params, [&](auto&& p) {
             auto&& name = p.first;
-            auto&& s = p.second;
+            auto&& s    = p.second;
 
             return to_array_type(s) + " " + name;
         });
@@ -182,33 +171,34 @@ struct generator
 
         auto return_ins = std::prev(prog->end());
 
-        for(auto ins:migraphx::iterator_for(*prog))
+        for(auto ins : migraphx::iterator_for(*prog))
         {
-            if (ins->name() == "@return")
+            if(ins->name() == "@return")
             {
                 assert(ins->inputs().size() == 1);
                 return_ins = ins->inputs().front();
             }
             std::string name = "z" + std::to_string(names.size());
-            if (ins->name() == "@param")
+            if(ins->name() == "@param")
             {
-                name = migraphx::any_cast<migraphx::builtin::param>(ins->get_operator()).parameter;   
+                name = migraphx::any_cast<migraphx::builtin::param>(ins->get_operator()).parameter;
             }
             names[ins] = name;
-            if (apply_map.count(ins->name()) > 0)
+            if(apply_map.count(ins->name()) > 0)
             {
                 std::string f = apply_map[ins->name()](ins);
                 ss << "auto " << name << " = " << f;
                 char delim = '(';
-                for(auto input:ins->inputs())
+                for(auto input : ins->inputs())
                 {
                     ss << delim << names.at(input);
                 }
                 ss << ");\n";
             }
-            else if (ins->name() == "@literal")
+            else if(ins->name() == "@literal")
             {
-                ss << to_array_type(ins->get_shape()) << " " << name << " = {" << ins->get_literal() << "};\n";
+                ss << to_array_type(ins->get_shape()) << " " << name << " = {" << ins->get_literal()
+                   << "};\n";
             }
         }
 
@@ -218,7 +208,6 @@ struct generator
 
         return fs.str() + ss.str();
     }
-
 };
 
 int main(int argc, const char* argv[])
